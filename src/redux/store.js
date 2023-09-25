957997eb0138f96e73fa98d72c85d39ad32d8f4f
Prajwal Kene <prajwalnk123@gmail.com>
2023-09-25 13:50:00 +0530

    store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 //single store for all
 
+// const redux =require("redux")
 import redux from 'redux';
 
 import { todoReducers } from './reducers/todoReducer';
 
-export const store = redux.createStore(todoReducers) 
+export const store = redux.createStore(todoReducers) ;
+
